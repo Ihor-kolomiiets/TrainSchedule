193dde97f94cc3997c6af2a62b936d736d348cfa
Speltzer <ihor.kolomiiets@gmail.com>
2018-07-18 17:18:16 +0300

    dd callback buttons

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import config
 import telebot
 # import dbworker
-# from telebot import types
+from telebot import types
 import GetSchedule
 
 bot = telebot.TeleBot(config.token)
@@ -9,7 +9,26 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Просто проверка работы\n' + GetSchedule.print_data())
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(types.InlineKeyboardButton(text='Півд. Півд-Зах. Дон. зал.', callback_data='South'))
+    keyboard.add(types.InlineKeyboardButton(text='Одеська зал.', callback_data='Odesa'))
+    keyboard.add(types.InlineKeyboardButton(text='Львівська зал.', callback_data='Lviv'))
+    keyboard.add(types.InlineKeyboardButton(text='Дніпропетр. зал.', callback_data='Dnipro'))
+    bot.send_message(message.chat.id, 'Оберіть регіон', reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    if call.data == 'South':
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.add(types.InlineKeyboardButton(text='Пошук по станції', callback_data='StationSearch'))
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                              text="Введите номер станции: ", reply_markup=keyboard)
+
+
+@bot.message_handler(func=lambda message: message.text)  # Need change to block before choose region
+def object_message(message):
+    bot.send_message(message.chat.id, "Result:\n" + GetSchedule.print_data(int(message.text)))
 
 
 if __name__ == '__main__':
