e7bee7aacde659af72c633d26ee278767908c00b
Speltzer <ihor.kolomiiets@gmail.com>
2018-07-21 16:19:36 +0300

    Add path for users db

--- config.py
+++ config.py
@@ -2,12 +2,13 @@ from enum import Enum
 
 token = '382546574:AAE2kuiu51P01BE9_5YnFlNGlsN2kY3rUTE'
 stations_database = 'trainSchedule.db'
+users_database = 'users.db'
 
 
 #  Класс состояний
 class States(Enum):
     S_START = '0'
-    S_FIRSTADD = '1'
+    S_STATIONSEARCH = '1'
     S_SECONDADD = '2'
     S_DELETEFIRST = '3'
     S_DELETESECOND = '4'
