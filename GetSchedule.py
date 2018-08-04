998b3c73842304acdb2e21945293a25897f6d7ab
Speltzer <ihor.kolomiiets@gmail.com>
2018-08-04 16:04:20 +0300

    Add function for print schedule

--- GetSchedule.py
+++ GetSchedule.py
@@ -10,12 +10,7 @@ def make_station_url(sid=1, sid2=0, lng=''):  # Function for make url to parse
         .format(sid, sid2, lng)
 
 
-def print_data(station_name):
-    station_id = dbworker.db_get(station_name)
-    if station_id is None:
-        return 'Нема такої станції, дебіл'
-    else:
-        station_id = station_id[0]
+def print_data(station_id):
     r = requests.get(make_station_url(station_id))
     soup = BeautifulSoup(r.text, 'lxml')
     result = soup.find('table', class_='td_center').find_all('tr', height='20')[2:]
--- bot.py
+++ bot.py
@@ -46,6 +46,16 @@ def callback_inline(call):
     dbworker.set_state(call.message.chat.id, config.States.S_START.value)
 
 
+@bot.callback_query_handler(func=lambda call: (True and call.data.startswith('Some_station ')))
+def send_message_with_station(call):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(types.InlineKeyboardButton(text='Назад', callback_data='Back_to_start'))
+    station_id = call.data.split(' ')
+    result_message = GetSchedule.print_data(station_id[1])
+    bot.send_message(call.message.chat.id, result_message, reply_markup=keyboard)
+    dbworker.set_state(call.message.chat.id, config.States.S_START.value)
+
+
 @bot.message_handler(func=lambda message: (message.text and
                                            dbworker.get_state(message.chat.id) == config.States.S_STATIONSEARCH.value))
 def station_search(message):
@@ -60,7 +70,7 @@ def station_search(message):
         # Переход по нужной станции производится нажатием на колбек кнопку с определенной информацией о станции
         for station in result:
             result_message += station[1] + '\n' + station[2] + '\n\n'
-            keyboard.add(types.InlineKeyboardButton(text=station[1], callback_data='Some_station' + str(station[0])))
+            keyboard.add(types.InlineKeyboardButton(text=station[1], callback_data='Some_station ' + str(station[0])))
         keyboard.add(types.InlineKeyboardButton(text='Назад', callback_data='Back_to_start'))
         bot.send_message(message.chat.id, result_message, reply_markup=keyboard)
 
